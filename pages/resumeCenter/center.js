191b73cc64aca3d274f3ac61123d5729b3f9763a
crazycattle <838872031@qq.com>
2018-02-28 14:19:31 +0800

    fix pages

--- pages/resumeCenter/center.js
+++ pages/resumeCenter/center.js
@@ -5,62 +5,198 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    page: 2,
+    fliterType: 'job',
+    // page 2
+    status: {
+      sending: '0',
+      seeing: '25%',
+      filtering: '50%',
+      inving: '75%',
+      seeing: '100%',
+    },
+    // page 3
+    id: 0,
+    ids: 1,
+    mockData: [
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      }
+    ],
+    // 职位推荐
+    jobList: [
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      },
+      {
+        pic_url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        job: 'JAVA研发工程师',
+        company: '上海脚步网络科技有限公司',
+        address: '上海',
+        educ: '本科',
+        data: '2018.01.24'
+      }
+    ]
   },
-
+  showDetail (e) {
+    let id = e.target.dataset.id
+    this.setData({
+      id: id
+    })
+  },
+  showStatus (e) {
+    let ids = e.target.dataset.ids
+    this.setData({
+      ids: ids
+    })
+  },
+  tabPage(e) {
+    let page = e.target.dataset.page
+    this.setData({
+      page: page
+    })
+  },
+  changeCokllectFilter (e) {
+    let type = e.target.dataset.type
+    this.setData({
+      fliterType: type
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
