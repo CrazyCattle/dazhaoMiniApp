9055d1e7a54727ebb7f5426a7e29cf45b44e4d9e
crazycattle <838872031@qq.com>
2018-03-01 16:57:40 +0800

    add notice detail page

--- pages/inforNoticeDetail/detail.js
+++ pages/inforNoticeDetail/detail.js
@@ -1,4 +1,4 @@
-// pages/inforNotice/notice.js
+// pages/inforNoticeDetail/detail.js
 Page({
 
   /**
@@ -7,11 +7,7 @@ Page({
   data: {
   
   },
-  linkDetail () {
-    wx.navigateTo({
-      url: '../inforNoticeDetail/detail'
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
